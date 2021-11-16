3257c2e5bb4f42c54f4e493dccfb3f718b2209ec
canusorn <anusorn1998@gmail.com>
2021-11-16 17:03:37 +0700

    add header,footer,logo

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,15 +1,33 @@
 </main>
-<script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.js">
-  </script>
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="">หน้าหลัก</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="user.php">ข้อมูลผู้ใช้ระบบ</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="">ข้อมูลการใช้งานระบบ</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="hospital.php">ข้อมูลโรงพยาบาล</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="patient.php">ข้อมูลผู้รักษาและผลการรักษา</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="">พิมพ์ข้อมูล</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="">Billing</a></li>
+      <li class="nav-item"><a class="nav-link px-2 text-muted" href="">ออกระบบ</a></li>
+
+    </ul>
+    <p class="text-center text-muted">© Copy Right 2021 N.B.Pathology Center</p>
+  </footer>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.js">
+</script>
 
 <script src="/js/script.js"></script>
-<br> :: Copy Right 2021 N.B.Pathology Center ::
+<!--<br> :: Copy Right 2021 N.B.Pathology Center ::
 </div>
+-->
 </body>
 
 
