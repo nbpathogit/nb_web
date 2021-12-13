570da8dc0e0eaf38c2ebb53b6c20ff542afacacc
canusorn <39732595+canusorn@users.noreply.github.com>
2021-12-13 19:05:21 +0700

    add some bootstrap form

--- includes/hospital_form.php
+++ includes/hospital_form.php
@@ -1,62 +1,27 @@
-<tbody><tr>
-                    <th height="40" bgcolor="#FFCCFF" scope="col"><div align="center" class="style6">
-                        <div align="left">&nbsp;<span class="style17">&nbsp;&nbsp;&nbsp;&nbsp;เพิ่มสถานพยาบาล</span></div>
-                    </div></th>
-                  </tr>
-                  <tr>
-                    <th height="264" scope="col">
-					     <form id="checkForm" name="checkForm" method="post" onsubmit="return check()" action="a_hospital_add2.php">
-                        <table width="99%" border="0" cellspacing="9" cellpadding="2">
-                          <tbody><tr>
-                            <th scope="col">&nbsp;</th>
-                            <th width="26%" class="style14" scope="col"><div align="right">ชื่อสถานพยาบาล</div></th>
-                            <th width="2%" scope="col">&nbsp;</th>
-                            <th width="68%" class="style200" scope="col"><div align="left">
-                                <input name="hospital_name" type="text" class="text4" id="hospital_name" size="50">
-                                <span class="comment">*</span></div></th>
-                            <th scope="col">&nbsp;</th>
-                          </tr>
-                          <tr>
-                            <th scope="col">&nbsp;</th>
-                            <th class="style14" scope="col"><div align="right">ที่อยู่</div></th>
-                            <th scope="col">&nbsp;</th>
-                            <th class="style200" scope="col"><div align="left">
-                                <textarea name="hospital_address" cols="60" rows="3" class="text4" id="hospital_address"></textarea>
-                                <span class="comment">*</span></div>
-                                <label></label></th>
-                            <th scope="col">&nbsp;</th>
-                          </tr>
-                          <tr>
-                            <th scope="col">&nbsp;</th>
-                            <th class="style14" scope="col"><div align="right">รายละเอียด</div></th>
-                            <th scope="col">&nbsp;</th>
-                            <th class="style200" scope="col"><label>
-                                <div align="left">
-                                  <textarea name="hospital_detail" cols="60" rows="2" class="text4" id="hospital_detail"></textarea>
-                                </div>
-                              </label></th>
-                            <th scope="col">&nbsp;</th>
-                          </tr>
-                          <tr>
-                            <th scope="col">&nbsp;</th>
-                            <th scope="col">&nbsp;</th>
-                            <th scope="col">&nbsp;</th>
-                            <th class="style200" scope="col">&nbsp;</th>
-                            <th scope="col">&nbsp;</th>
-                          </tr>
-                          <tr>
-                            <th width="2%" scope="col">&nbsp;</th>
-                            <th colspan="3" class="style200" scope="col"><table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                <tbody><tr>
-                                  <th width="32%" scope="col"><br></th>
-                                  <th width="12%" scope="col"><input name="Submit2" type="submit" class="BUT_G_78_23_L" value="เพิ่มข้อมูล"></th>
-                                  <th width="12%" scope="col"><input name="Reset" type="reset" class="BUT_G_78_23_L" id="Reset" value="ยกเลิก"></th>
-                                  <th width="44%" scope="col"><div align="left"></div></th>
-                                </tr>
-                            </tbody></table></th>
-                            <th width="2%" scope="col">&nbsp;</th>
-                          </tr>
-                        </tbody></table>
-                    </form></th>
-                  </tr>
-                </tbody>
+<h4>เพิ่มสถานพยาบาล</h4>
+
+
+<form class="row g-2" id="checkForm" name="checkForm" method="post" onsubmit="return check()" action="a_hospital_add2.php">
+
+  <div class="mb-3">
+    <label for="hospital_name" class="form-label">ชื่อสถานพยาบาล</label>
+    <input type="text" class="form-control" id="hospital_name">
+  </div>
+
+  <div class="mb-3">
+    <label for="hospital_address" class="form-label">ที่อยู่</label>
+    <textarea name="hospital_address" class="form-control" id="hospital_address" rows="3"></textarea>
+  </div>
+
+  <div class="mb-3">
+    <label for="hospital_address" class="form-label">รายละเอียด</label>
+    <textarea name="hospital_detail" class="form-control" id="hospital_detail" rows="2"></textarea>
+  </div>
+
+
+  <button name="Submit2" type="submit" class="btn btn-primary">เพิ่มข้อมูล</button>
+  <button name="Reset" type="reset" class="btn btn-primary" id="Reset">ยกเลิก</button>
+
+</form>
+
+<br>
--- user.php
+++ user.php
@@ -22,37 +22,77 @@ $hospitals = Hospital::getAll($conn);
 
 <hr>
 <!--<table class="table table-hover table-striped"  border="1" >-->
-<table border="1" align="center" >
+<table class="table table-hover table-striped">
     <thead>
-        <tr >
-            <td><div align="center">id</div></td>
-            <td><div align="center">name</div></td>
-            <td><div align="center">lastname</div></td>
-            <td><div align="center">username</div></td>
-            <td><div align="center">password</div></td>
-            <td><div align="center">hospital</div></td>
-            <td><div align="center">group</div></td>
-            <td><div align="center">Detail</a</div></td>
-            <td><div align="center">Edit</div></td>
-            <td><div align="center">Delete</div></td>
+        <tr>
+            <td>
+                <div align="center">id</div>
+            </td>
+            <td>
+                <div align="center">name</div>
+            </td>
+            <td>
+                <div align="center">lastname</div>
+            </td>
+            <td>
+                <div align="center">username</div>
+            </td>
+            <td>
+                <div align="center">password</div>
+            </td>
+            <td>
+                <div align="center">hospital</div>
+            </td>
+            <td>
+                <div align="center">group</div>
+            </td>
+            <td>
+                <div align="center">Detail</a< /div>
+            </td>
+            <td>
+                <div align="center">Edit</div>
+            </td>
+            <td>
+                <div align="center">Delete</div>
+            </td>
         </tr>
     </thead>
     <tbody>
-<?php foreach ($users as $user): ?>
-            <tr >
-                <td><div align="center"><?= $user['uid']; ?></div></td>
-                <td><div align="center"><?= $user['name']; ?></div></td>
-                <td><div align="center"><?= $user['lastname']; ?></div></td>
-                <td><div align="center"><?= $user['username']; ?></div></td>
-                <td><div align="center"><?= $user['password']; ?></div></td>
-                <td><div align="center"><?= $user['hospital']; ?></div></td>
-                <td><div align="center"><?= $user['ugroup']; ?></div></td>
-                <td><div align="center"><a href="\user_detail.php?id=<?= $user['uid']; ?>">Detail</a></div></td>
-                <td><div align="center"><a href="hospital_edit.php">Edit</a></div></td>
-                <td><div align="center"><a href="hospital_del.php">Delete</a></div></td>
+        <?php foreach ($users as $user) : ?>
+            <tr>
+                <td>
+                    <div align="center"><?= $user['uid']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['name']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['lastname']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['username']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['password']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['hospital']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><?= $user['ugroup']; ?></div>
+                </td>
+                <td>
+                    <div align="center"><a href="\user_detail.php?id=<?= $user['uid']; ?>">Detail</a></div>
+                </td>
+                <td>
+                    <div align="center"><a href="hospital_edit.php">Edit</a></div>
+                </td>
+                <td>
+                    <div align="center"><a href="hospital_del.php">Delete</a></div>
+                </td>
             </tr>
-<?php endforeach; ?>
+        <?php endforeach; ?>
         </thead>
 </table>
 
-        <?php require 'includes/footer.php'; ?>
+<?php require 'includes/footer.php'; ?>
