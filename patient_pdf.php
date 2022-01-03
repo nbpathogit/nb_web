b12e0d54d0f10e8eebde4719c7bd2866be98ae49
Anuchit Chumthong <anuchit.chu@wdc.com>
2022-01-03 16:31:26 +0700

    20210103 16 31

---
+++ patient_pdf.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//custom font
+$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+$fontDirs = $defaultConfig['fontDir'];
+
+$defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+$fontData = $defaultFontConfig['fontdata'];
+
+$mpdf = new \Mpdf\Mpdf([
+    'fontDir' => array_merge($fontDirs, [
+        __DIR__ . '/fonts',
+    ]),
+    'fontdata' => $fontData + [
+            'sarabun' => [
+                'R' => 'THSarabunNew.ttf',
+                'I' => 'THSarabunNew Italic.ttf',
+                'B' =>  'THSarabunNew Bold.ttf',
+            ]
+        ],
+]);
+
+
+$content = file_get_contents('includes/format_pdf.php');
+
+$mpdf->WriteHTML($content);
+$mpdf->Output();
+?>
