4c060c6cefdf629c245b25daff7a32a5c466d97d
Anuchit Chumthong <anuchit.chu@wdc.com>
2022-01-03 19:39:57 +0700

    20220103 19 39

--- patient_pdf.php
+++ patient_pdf.php
@@ -9,21 +9,31 @@ $fontDirs = $defaultConfig['fontDir'];
 $defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
 $fontData = $defaultFontConfig['fontdata'];
 
-$mpdf = new \Mpdf\Mpdf([
-    'fontDir' => array_merge($fontDirs, [
-        __DIR__ . '/fonts',
-    ]),
-    'fontdata' => $fontData + [
-            'sarabun' => [
-                'R' => 'THSarabunNew.ttf',
-                'I' => 'THSarabunNew Italic.ttf',
-                'B' =>  'THSarabunNew Bold.ttf',
-            ]
-        ],
-]);
+//$mpdf = new \Mpdf\Mpdf([
+//    'fontDir' => array_merge($fontDirs, [
+//        __DIR__ . '/fonts',
+//    ]),
+//    'fontdata' => $fontData + [
+//            'sarabun' => [
+//                'R' => 'THSarabunNew.ttf',
+//                'I' => 'THSarabunNew Italic.ttf',
+//                'B' =>  'THSarabunNew Bold.ttf',
+//            ]
+//        ],
+//]);
+$mpdf = new \Mpdf\Mpdf(array(
+//        'format' => array(57,160),
+        'mode'   => 'utf-8',
+        'default_font' => 'angsana',
+        'default_font_size' => 18,
+//        'tempDir' => '/tmp',
+//        'margin_left' => 4,
+//        'margin_right' => 6,
+//        'margin_top' => 1,
+//        'margin_bottom' => 5,
+        ));
 
-
-$content = file_get_contents('includes/format_pdf.php');
+$content = file_get_contents('patient_format_pdf.php');
 
 $mpdf->WriteHTML($content);
 $mpdf->Output();
