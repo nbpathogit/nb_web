f3df5a879c099578d6611b0c07947b8cfe449899
canusorn <39732595+canusorn@users.noreply.github.com>
2022-02-21 10:17:52 +0700

    specimen search by javascript

--- specimen.php
+++ specimen.php
@@ -5,20 +5,20 @@ require 'includes/init.php';
 
 $conn = require 'includes/db.php';
 
-// $specimens = Specimen::getAll($conn);
-if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
+$specimens = Specimen::getAll($conn);
+// if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
 
-    // var_dump($_REQUEST);
+//     // var_dump($_REQUEST);
 
-    // $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
-    // var_dump($paginator);
-    $specimens = Specimen::getSearch($conn, $_REQUEST['search']);
-    // var_dump($specimens);
-} else {
-    $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
-    // var_dump($paginator);
-    $specimens = Specimen::getPage($conn, $paginator->limit, $paginator->offset);
-}
+//     // $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
+//     // var_dump($paginator);
+//     $specimens = Specimen::getSearch($conn, $_REQUEST['search']);
+//     // var_dump($specimens);
+// } else {
+//     $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
+//     // var_dump($paginator);
+//     $specimens = Specimen::getPage($conn, $paginator->limit, $paginator->offset);
+// }
 ?>
 
 
@@ -31,28 +31,66 @@ if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
 <?php else : ?>
 
 
-    <!-- search form -->
-    <form id="" name="" method="post">
+    <style type="text/css">
+        .pagination {
+            margin: 0;
+        }
 
-        <div class="mb-4">
-            <h4><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg> ค้นหาข้อมูล</h4>
+        .pagination li:hover {
+            cursor: pointer;
+        }
+
+        .header_wrap {
+            padding: 30px 0;
+        }
+
+        .num_rows {
+            width: 20%;
+            float: left;
+        }
+
+        .tb_search {
+            width: 20%;
+            float: right;
+        }
+
+        .pagination-container {
+            width: 70%;
+            float: left;
+        }
+
+        .rows_count {
+            width: 20%;
+            float: right;
+            text-align: right;
+            color: #999;
+        }
+    </style>
+
+    <div class="header_wrap">
+        <div class="num_rows">
+
+            <div class="form-group">
+                <!--		Show Numbers Of Rows 		-->
+                <select class="form-control" name="state" id="maxRows">
+
+
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="5000">Show ALL Rows</option>
+                </select>
+
+            </div>
         </div>
-
-        <div class="mb-3">
-            <label for="search" class="form-label">คำที่ต้องการค้นหา</label>
-            <input name="search" type="text" class="form-control" id="search" value="<?= isset($_REQUEST['search']) ? $_REQUEST['search'] : ""  ?>">
+        <div class="tb_search">
+            <input type="text" id="search_input_all" onkeyup="FilterkeyWord_all_table()" placeholder="Search.." class="form-control">
         </div>
-
-        <div class="mb-3">
-            <input name="" type="submit" class="btn btn-primary" value="ค้นหา" size="50">
-        </div>
-
-    </form>
+    </div>
 
     <hr>
-    <table class="table table-hover table-striped text-center">
+    <table class="table table-hover table-striped text-center" id="table-id">
         <thead>
             <tr>
                 <th scope="col">#</th>
@@ -72,14 +110,26 @@ if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
             <?php endforeach; ?>
             </thead>
     </table>
-
-    <?php if (isset($paginator)) require 'includes/pagination.php'; ?>
+    
+<!--		Start Pagination -->
+<div class='pagination-container'>
+        <nav>
+            <ul class="pagination">
+                <!--	Here the JS Function Will Add the Rows -->
+            </ul>
+        </nav>
+    </div>
+    <div class="rows_count"></div>
 
 <?php endif; ?>
 
 <?php require 'includes/footer.php'; ?>
 
+
+
 <script type="text/javascript">
+
+    // delete
     $("a.delete").on("click", function(e) {
 
         e.preventDefault();
@@ -93,4 +143,148 @@ if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
             frm.submit();
         }
     });
+
+
+    //--------------------------------------------------------------------//
+
+    // table search
+    getPagination('#table-id');
+    $('#maxRows').trigger('change');
+
+    function getPagination(table) {
+
+        $('#maxRows').on('change', function() {
+            $('.pagination').html(''); // reset pagination div
+            var trnum = 0; // reset tr counter 
+            var maxRows = parseInt($(this).val()); // get Max Rows from select option
+
+            var totalRows = $(table + ' tbody tr').length; // numbers of rows 
+            $(table + ' tr:gt(0)').each(function() { // each TR in  table and not the header
+                trnum++; // Start Counter 
+                if (trnum > maxRows) { // if tr number gt maxRows
+
+                    $(this).hide(); // fade it out 
+                }
+                if (trnum <= maxRows) {
+                    $(this).show();
+                } // else fade in Important in case if it ..
+            }); //  was fade out to fade it in 
+            if (totalRows > maxRows) { // if tr total rows gt max rows option
+                var pagenum = Math.ceil(totalRows / maxRows); // ceil total(rows/maxrows) to get ..  
+                //	numbers of pages 
+                for (var i = 1; i <= pagenum;) { // for each page append pagination li 
+                    $('.pagination').append('<li class="page-item" data-page="' + i + '">\
+								      <span class="page-link">' + i++ + '</span>\
+								    </li>').show();
+                } // end for i 
+
+            } // end if row count > max rows
+            $('.pagination li:first-child').addClass('active'); // add active class to the first li 
+
+            //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
+            showig_rows_count(maxRows, 1, totalRows);
+            //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
+
+            $('.pagination li').on('click', function(e) { // on click each page
+                e.preventDefault();
+                var pageNum = $(this).attr('data-page'); // get it's number
+                var trIndex = 0; // reset tr counter
+                $('.pagination li').removeClass('active'); // remove active class from all li 
+                $(this).addClass('active'); // add active class to the clicked 
+
+                //SHOWING ROWS NUMBER OUT OF TOTAL
+                showig_rows_count(maxRows, pageNum, totalRows);
+                //SHOWING ROWS NUMBER OUT OF TOTAL
+
+                $(table + ' tr:gt(0)').each(function() { // each tr in table not the header
+                    trIndex++; // tr index counter 
+                    // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
+                    if (trIndex > (maxRows * pageNum) || trIndex <= ((maxRows * pageNum) - maxRows)) {
+                        $(this).hide();
+                    } else {
+                        $(this).show();
+                    } //else fade in 
+                }); // end of for each tr in table
+            }); // end of on click pagination list
+        });
+        // end of on select change 
+        // END OF PAGINATION 
+    }
+
+
+
+    // SI SETTING
+    $(function() {
+        // Just to append id number for each row  
+        default_index();
+
+    });
+
+    //ROWS SHOWING FUNCTION
+    function showig_rows_count(maxRows, pageNum, totalRows) {
+        //Default rows showing
+        var end_index = maxRows * pageNum;
+        var start_index = ((maxRows * pageNum) - maxRows) + parseFloat(1);
+        var string = 'Showing ' + start_index + ' to ' + end_index + ' of ' + totalRows + ' entries';
+        $('.rows_count').html(string);
+    }
+
+    // CREATING INDEX
+    function default_index() {
+        $('table tr:eq(0)').prepend('<th> ID </th>')
+
+        var id = 0;
+
+        $('table tr:gt(0)').each(function() {
+            id++
+            $(this).prepend('<td>' + id + '</td>');
+        });
+    }
+
+    // All Table search script
+    function FilterkeyWord_all_table() {
+
+        // Count td if you want to search on all table instead of specific column
+
+        var count = $('.table').children('tbody').children('tr:first-child').children('td').length;
+
+        // Declare variables
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("search_input_all");
+        var input_value = document.getElementById("search_input_all").value;
+        filter = input.value.toLowerCase();
+        if (input_value != '') {
+            table = document.getElementById("table-id");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 1; i < tr.length; i++) {
+
+                var flag = 0;
+
+                for (j = 0; j < count; j++) {
+                    td = tr[i].getElementsByTagName("td")[j];
+                    if (td) {
+
+                        var td_text = td.innerHTML;
+                        if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+                            //var td_text = td.innerHTML;  
+                            //td.innerHTML = 'shaban';
+                            flag = 1;
+                        } else {
+                            //DO NOTHING
+                        }
+                    }
+                }
+                if (flag == 1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        } else {
+            //RESET TABLE
+            $('#maxRows').trigger('change');
+        }
+    }
 </script>
