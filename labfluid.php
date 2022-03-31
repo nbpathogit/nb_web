34f19790ed3d6fc5de3bd2c0d62a0899b4055999
canusorn <39732595+canusorn@users.noreply.github.com>
2022-03-31 11:51:05 +0700

    lab fluid

--- labfluid.php
+++ labfluid.php
@@ -5,20 +5,6 @@ require 'includes/init.php';
 
 $conn = require 'includes/db.php';
 
-// $specimens = Specimen::getAll($conn);
-// if (isset($_REQUEST['search']) && $_REQUEST['search'] != "") {
-
-//     // var_dump($_REQUEST);
-
-//     // $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
-//     // var_dump($paginator);
-//     $specimens = Specimen::getSearch($conn, $_REQUEST['search']);
-//     // var_dump($specimens);
-// } else {
-//     $paginator = new Paginator(isset($_GET['page']) ? $_GET['page'] : 1, 30, Specimen::getTotal($conn));
-//     // var_dump($paginator);
-//     $specimens = Specimen::getPage($conn, $paginator->limit, $paginator->offset);
-// }
 ?>
 
 
@@ -34,7 +20,7 @@ $conn = require 'includes/db.php';
         <?php else : ?>
 
             <div class="d-flex align-items-center justify-content-between">
-                <a href="/specimen_add.php" class="btn btn-outline-primary m-2 mb-0"><i class="fa-solid fa-disease me-2"></i>เพิ่มสิ่งส่งตรวจ</a>
+                <a href="/labfluid_add.php" class="btn btn-outline-primary m-2 mb-0"><i class="fa-solid fa-disease me-2"></i>เพิ่ม Fluid</a>
             </div>
 
     </div>
@@ -43,11 +29,12 @@ $conn = require 'includes/db.php';
 <div class="container-fluid pt-4 px-4">
     <div class="row bg-light rounded align-items-center justify-content-center p-3 mx-1">
 
-            <table class="table table-hover table-striped text-center" id="specimen_table" style="width:100%">
+            <table class="table table-hover table-striped text-center" id="fluid_table" style="width:100%">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Specimen</th>
+                        <th scope="col">Fluid</th>
+                        <th scope="col">Detail</th>
                         <th scope="col">Manage</th>
                     </tr>
                 </thead>
@@ -66,14 +53,14 @@ $conn = require 'includes/db.php';
 
 
         // table data
-        var table = $('#specimen_table').DataTable({
-            "ajax": "data/specimen.php?skey=<?= $_SESSION["skey"]; ?>",
+        var table = $('#fluid_table').DataTable({
+            "ajax": "data/labfluid.php?skey=<?= $_SESSION["skey"]; ?>",
             responsive: true,
             columnDefs: [
                 {
                     "render": function(data, type, row) {
                         // return data + ' (' + row[3] + ')';
-                        return '<a href="specimen_edit.php?id=' + row[0] + '" class="btn btn-outline-primary btn-sm me-1 edit"><i class="fa-solid fa-marker"></i></a><a href="specimen_del.php?id=' + row[0] + '" class="btn btn-outline-dark btn-sm delete"><i class="fa-solid fa-trash-can"></i></a>';
+                        return '<a href="labfluid_edit.php?id=' + row[0] + '" class="btn btn-outline-primary btn-sm me-1 edit"><i class="fa-solid fa-marker"></i></a><a href="labfluid_del.php?id=' + row[0] + '" class="btn btn-outline-dark btn-sm delete"><i class="fa-solid fa-trash-can"></i></a>';
                     },
                     "targets": -1
                 },
@@ -81,20 +68,20 @@ $conn = require 'includes/db.php';
         });
 
         // delete user
-        $('#specimen_table tbody').on('click', 'a.delete', function(e) {
+        $('#fluid_table tbody').on('click', 'a.delete', function(e) {
             var data = table.row($(this).parents('tr')).data();
 
             e.preventDefault();
             if (confirm("Are you sure?")) {
                 var frm = $("<form>");
                 frm.attr('method', 'post');
-                frm.attr('action', "specimen_del.php?id=" + data[0]);
+                frm.attr('action', "labfluid_del.php?id=" + data[0]);
                 frm.appendTo("body");
                 frm.submit();
             }
         });
 
         // set active tab
-        $("#specimen").addClass("active");
+        $("#fluid").addClass("active");
     });
 </script>
---
+++ labfluid_add.php
@@ -0,0 +1,68 @@
+<?php
+require 'includes/init.php';
+
+//Auth::requireLogin();
+
+$conn = require 'includes/db.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $fluid = new LabFluid();
+    $fluid->id = $_GET['id'];
+    $fluid->labname = $_POST['labname'];
+    $fluid->lab_des = $_POST['lab_des'];
+
+    if ($fluid->create($conn)) {
+
+        Url::redirect("/labfluid.php");
+    } else {
+        echo '<script>alert("Add fluid fail. Please verify again")</script>';
+    }
+}
+?>
+
+
+<?php require 'includes/header.php'; ?>
+
+<div class="container-fluid pt-4 px-4">
+    <div class="row bg-light rounded align-items-center justify-content-center p-3 mx-1">
+
+
+        <?php if (!Auth::isLoggedIn()) : ?>
+            You are not login.
+        <?php else : ?>
+            <h4>เพิ่ม Fluid</h4>
+
+            <form class="row g-2" method="post">
+
+                <div class="mb-3">
+                    <label for="labname" class="form-label">Fluid</label>
+                    <input name="labname" type="text" class="form-control" id="labname">
+                </div>
+
+                <div class="mb-3">
+                    <label for="lab_des" class="form-label">Fluid detail</label>
+                    <textarea name="lab_des" class="form-control" id="lab_des" rows="3"></textarea>
+                </div>
+
+                <div class="d-grid gap-2 d-md-block">
+                    <button name="Submit" type="submit" class="btn btn-primary">แก้ไขข้อมูล</button>
+                    <button name="Reset" type="reset" class="btn btn-secondary" id="Reset">ยกเลิก</button>
+                </div>
+
+            </form>
+
+        <?php endif; ?>
+
+
+    </div>
+</div>
+
+
+<?php require 'includes/footer.php'; ?>
+
+<script type="text/javascript">
+    //set active tab
+    $("#fluid").addClass("active");
+</script>
