c2c338a15d2f54db9789e6c34f79d0c367dd4a74
Anuchit Chumthong <94755581+anuchitchu@users.noreply.github.com>
2022-10-18 21:43:59 +0700

    20221018_214340  Add Util Prepend function

--- classes/Util.php
+++ classes/Util.php
@@ -6,7 +6,60 @@
  * @author 
  */
 class Util {
+
     public static function isMobile() {
         return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
     }
+
+    // Function to prepend a string 
+    // 
+    //input: int(5) , string(1) "1"
+    //Output: string(5) "00001"
+    //
+    //input: int(5) , string(2) "11"
+    //Output: string(5) "00011"
+    //
+    //input: int(5) , string(3) "111"
+    //Output: string(5) "00111"
+    // 
+    //input: int(5) , string(4) "1111"
+    //Output: string(5) "01111"  
+    //
+    //input: int(5) , string(5) "11111"
+    //Output: string(5) "11111"
+    //
+    //input: int(5) , string(6) "111111"
+    //Output: string(6) "111111"
+    //
+    ////////////////////////////
+    function prepend_string_with_zero($numdigit, $runstr) {
+        $res = "null";
+        // Using concatenation operator (.)
+        if (strlen($runstr) >= $numdigit) {
+            $res = $runstr;
+        } elseif ($numdigit - strlen($runstr) == 1) {
+            $res = "0" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 2) {
+            $res = "00" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 3) {
+            $res = "000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 4) {
+            $res = "0000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 5) {
+            $res = "00000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 6) {
+            $res = "000000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 7) {
+            $res = "0000000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 8) {
+            $res = "00000000" . $runstr;
+        } elseif ($numdigit - strlen($runstr) == 9) {
+            $res = "00000000" . $runstr;
+        }
+
+
+        // Returning the result 
+        return $res;
+    }
+
 }
---
+++ sandbox/test_datetime.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+date_default_timezone_set('Asia/Bangkok');
+
+// Return current date from the remote server
+echo date('d-m-y h:i:s');
+echo '<br>';
+// Prints the day, date, month, year, time, AM or PM
+echo date("l jS \of F Y h:i:s A");
+echo '<br>';
+echo date('y');
+echo '<br>';
+
+?>
