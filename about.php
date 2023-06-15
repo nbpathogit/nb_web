8c75808254b119f950b15952ed341293d4506235
canusorn <39732595+canusorn@users.noreply.github.com>
2023-06-15 16:22:40 +0700

    about and webfeature

--- about.php
+++ about.php
@@ -6,29 +6,39 @@ require 'includes/init.php';
 <?php require 'user_auth.php'; ?>
 <?php require 'includes/header.php'; ?>
 
-<div class="container-md">
-    <h3>เกี่ยวกับเรา</h3>
-    <p class="text-break">คลินิกเฉพาะทางด้านเวชกรรมพยาธิวิทยากายวิภาค ศูนย์พยาธิวิทยา เอ็น. บี.ในนาม ห้างหุ้นส่วนจำกัด ศูนย์พยาธิวิทยา เอ็น.บี. ให้บริการตรวจวิเคราะห์และวินิจฉัยโรคทางห้องปฏิบัติการพยาธิวิทยากายวิภาค โดยเปิดรับบริการแก่ หน่วยงานทั้งภาครัฐและเอกชน
-    </p>
 
-    <div class="bd-example">
-        <address>
-            <strong>ตั้งอยู่</strong><br>
-            18/54 ซอย แป้นวงศ์<br>
-            ถนนสนามบิน ตำบลในเมือง อำเภอเมือง จังหวัดพิษณุโลก 65000<br>
-            <abbr title="Phone"><strong>โทรศัพท์</strong><br></abbr>085-040-1841 และ 064-354-5283
-        </address>
-        <address>
-            <strong>Email</strong><br>
-            <a href="mailto:nbpathologyplk@gmail.com">nbpathologyplk@gmail.com</a>
-        </address>
-    </div>
+<div class="container-fluid pt-4 px-4">
+    <!-- <div class="row vh-100 bg-light rounded justify-content-center mx-0"> -->
+
+        <div class="container-md bg-blue-a">
+            <h4>เกี่ยวกับเรา</h4>
+            <p class="text-break">คลินิกเฉพาะทางด้านเวชกรรมพยาธิวิทยากายวิภาค ศูนย์พยาธิวิทยา เอ็น. บี.ในนาม ห้างหุ้นส่วนจำกัด ศูนย์พยาธิวิทยา เอ็น.บี. ให้บริการตรวจวิเคราะห์และวินิจฉัยโรคทางห้องปฏิบัติการพยาธิวิทยากายวิภาค โดยเปิดรับบริการแก่ หน่วยงานทั้งภาครัฐและเอกชน
+            </p>
+
+            <div class="bd-example">
+                <address>
+                    <strong>ตั้งอยู่</strong><br>
+                    18/54 ซอย แป้นวงศ์<br>
+                    ถนนสนามบิน ตำบลในเมือง อำเภอเมือง จังหวัดพิษณุโลก 65000<br>
+                    <abbr title="Phone"><strong>โทรศัพท์</strong><br></abbr>085-040-1841 และ 064-354-5283
+                </address>
+                <address>
+                    <strong>Email</strong><br>
+                    <a href="mailto:nbpathologyplk@gmail.com">nbpathologyplk@gmail.com</a>
+                </address>
+            </div>
+        </div>
+
+    <!-- </div> -->
 </div>
+
+
+
 
 <?php require 'includes/footer.php'; ?>
 
 <script type="text/javascript">
-//set active tab
-    $( "#about_main" ).addClass( "active" );
-    $( "#about" ).addClass( "active" );
+    //set active tab
+    $("#about_main").addClass("active");
+    $("#about").addClass("active");
 </script>
