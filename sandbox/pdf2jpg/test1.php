ff6e7fdeec97f3968227d9594910f4ff020a3cbd
Anuchit Chumthong <94755581+anuchitchu@users.noreply.github.com>
2023-09-20 14:35:08 +0700

    sandbox pdf to jpg

--- sandbox/pdf2jpg/test1.php
+++ sandbox/pdf2jpg/test1.php
@@ -1,17 +1,35 @@
 <?php
 
+$os = PHP_OS;
+if ($os == "WINNT") {
+    echo "Window Process<br>";
+} else if ($os == "Linux") {
+    echo "Linux Process<br>";
+} else {
+    echo "Dont know OS:" . $os;
+    die();
+}
+
 echo "a<br>";
+//$output = exec('magick  -density 300 a.pdf -density 300 -quality 100 bb.jpg');
+$output = null;
+$retval = null;
+if ($os == "WINNT") {
+    $command = 'magick -density 300  a.pdf -density 300 bb.jpg';
+}
+if ($os == "Linux") {
+    $command = '/usr/local/bin/magick -density 300  a.pdf -density 300 bb.jpg';
+}
+//$command = 'whoami';
+if (exec($command, $output, $retval) == 0) {
+    echo 'execute command "'. $command . '" successful.<br>';
+    echo "Returned with status $retval and output:\n<br>";
+    print_r($output);
+} else {
+    echo 'execute command "'. $command . '" successful.<br>';
+    echo "Returned with status $retval and output:\n<br>";
+    print_r($output);
+}
 
-
-
-//$output = exec('magick -units PixelsPerInch -density 300 a.pdf -density 300 -quality 100 bb.jpg');
-$output=null;
-$retval=null;
-$command = 'magick -units PixelsPerInch -density 300 a.pdf -density 300 -quality 100 bb.jpg';
-exec($command, $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
-
-echo "b<br>";
 
 
