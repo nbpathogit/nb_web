570459206c4197883ed44f26c7cb4c79e4dcf6a6
canusorn <anusorn1998@gmail.com>
2025-07-22 21:47:43 +0700

    add documents

--- doc_download.php
+++ doc_download.php
@@ -10,7 +10,7 @@ require 'includes/init.php';
         <a href="file/ใบส่งชิ้นเนื้อศูนย์เอ็นบี.pdf" target="_blank"><h5 class="font-base fs-lg-0 fs-xl-1 my-3"><i class="fa-solid fa-circle-down"></i>ใบส่งชิ้นเนื้อศูนย์เอ็นบี</h5></a>
         <a href="file/ใบส่งตรวจสารน้ำทางเซลล์วิทยา.pdf" target="_blank"><h5 class="font-base fs-lg-0 fs-xl-1 my-3"><i class="fa-solid fa-circle-down"></i>ใบส่งตรวจสารน้ำทางเซลล์วิทยา</h5></a>
         <a href="file/คู่มือการใช้งานสำหรับลูกค้า_20231217.pdf" target="_blank"><h5 class="font-base fs-lg-0 fs-xl-1 my-3"><i class="fa-solid fa-circle-down"></i>คู่มือการใช้งานสำหรับลูกค้า</h5></a>
-
+        <a href="file/คู่มือส่งตรวจทางห้องปฏิบัติการ 68.pdf" target="_blank"><h5 class="font-base fs-lg-0 fs-xl-1 my-3"><i class="fa-solid fa-circle-down"></i>คู่มือการส่งตรวจทางพยาธิวิทยา</h5></a>
 
     </div>
 </div>
