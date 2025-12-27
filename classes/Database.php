e7e491eea3e57db5ff59d66d1a0b314d3851aaa0
nbpathogit <93444792+nbpathogit@users.noreply.github.com>
2025-12-27 21:46:59 +0700

    update Dababase.php

--- classes/Database.php
+++ classes/Database.php
@@ -9,12 +9,18 @@
  */
 class Database {
 
-    public $db_host = "";
-    public $db_name = "";
-    public $db_user = "";
-    public $db_pass = "";
+    private string  $db_host="";
+    private string  $db_name="";
+    private string  $db_user="";
+    private string  $db_pass="";
     
     public function __construct() {
+        
+        //defulte
+        $this->db_host = "xxxx";
+        $this->db_name = "xxxx";
+        $this->db_user = "xxxx";
+        $this->db_pass = "xxxx";
 
         if ($_SERVER['HTTP_HOST'] == "localhost") {
 
@@ -45,24 +51,40 @@ class Database {
             $this->db_user = "u189879599_nb";
             $this->db_pass = "tO22dE^4&$";
         }
+
+        if ($_SERVER['HTTP_HOST'] == "nbpathology.com"  || $_SERVER['HTTP_HOST'] == "www.nbpathology.com") {
+
+            if (Url::getSubfolder() != "") {
+                $this->db_host = "localhost";
+                $this->db_name = "nbpa_data" . "_" . Url::getSubfolder();
+                $this->db_user = "nbpa_nbpatho";
+                $this->db_pass = "@6i3#xdyJusmDfBJ";
+            } else {
+                $this->db_host = "localhost";
+                $this->db_name = "nbpa_data";
+                $this->db_user = "nbpa_nbpatho";
+                $this->db_pass = "@6i3#xdyJusmDfBJ";
+            }
+        }
+
     }
 
  
 
     public function getDBHost() {
-        return self::$db_host;
+        return $this->db_host;
     }
 
     public function getDBName() {
-        return self::$db_name;
+        return $this->db_name; 
     }
 
     public function getDBUser() {
-        return self::$db_user;
+        return $this->db_user;
     }
 
     public function getDBPass() {
-        return self::$db_pass;
+        return $this->db_pass;
     }
 
     /**
